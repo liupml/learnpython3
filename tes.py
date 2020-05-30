13435a5266fdb73f7cc26d7907e442bd37e7ac79
liu <354832357@qq.com>
2020-05-30 16:09:44 +0800

    python3 learn

---
+++ tes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def foo():
+    print("starting...")
+    while True:
+        res = yield 4
+        print("res:",res)
+        yield 5
+g = foo()
+print('bgin')
+print(next(g))
+print("*"*20)
+print(next(g))
+print("*"*20)
+print(next(g))
